ecbd5c9b6d6a98d71419c75802328157ad560529
Ashock0114 <145480991+Ashock0114@users.noreply.github.com>
2024-01-16 16:55:13 +0000

    Feesh

--- public/main.js
+++ public/main.js
@@ -1,5 +1,11 @@
 // Write JavaScript here.
-document.getElementById("fish")
-.addEventListener(
-  'click', function(MouseEvent) {this.style.cursor = "url('path/to/Feesh.png'), auto";
+
+
+let Feesh = document.querySelector("#fish");
+Feesh.style.position = "absolute";
+let body = document.querySelector("body");
+body.addEventListener("mousemove", function (event) {
+  Feesh.style.left = event.pageX + "px";
+  Feesh.style.top = event.pageY + "px";
+Feesh.style.pointerEvents = "none"
 });
